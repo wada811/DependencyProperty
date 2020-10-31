de945b3631d511b977f5cb92bcc97e2611a4e870
wada811 <at.wada811@gmail.com>
2020-10-31 23:22:07 +0900

    Improve performance of DependencyProperty

--- DependencyProperty/src/main/java/com/wada811/dependencyproperty/DependencyProperty.kt
+++ DependencyProperty/src/main/java/com/wada811/dependencyproperty/DependencyProperty.kt
@@ -11,29 +11,29 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.work.ListenableWorker
 
 @Deprecated("Use dependency<T, R>(resolve)", ReplaceWith("this.dependency<T, R>(resolve)"), DeprecationLevel.WARNING)
-fun <T : DependencyModule, R> Application.dependency(clazz: Class<T>, resolve: T.() -> R): Lazy<R> = lazy { dependencyModule(clazz).resolve() }
+fun <T : DependencyModule, R> Application.dependency(clazz: Class<T>, resolve: T.() -> R): Lazy<R> = lazy(LazyThreadSafetyMode.NONE) { dependencyModule(clazz).resolve() }
 inline fun <reified T : DependencyModule, R> Application.dependency(noinline resolve: (T) -> R): Lazy<R> = dependency(T::class.java, resolve)
 
 @Deprecated("Use dependency<T, R>(resolve)", ReplaceWith("this.dependency<T, R>(resolve)"), DeprecationLevel.WARNING)
-fun <T : DependencyModule, R> FragmentActivity.dependency(clazz: Class<T>, resolve: T.() -> R): Lazy<R> = lazy { dependencyModule(clazz).resolve() }
+fun <T : DependencyModule, R> FragmentActivity.dependency(clazz: Class<T>, resolve: T.() -> R): Lazy<R> = lazy(LazyThreadSafetyMode.NONE) { dependencyModule(clazz).resolve() }
 inline fun <reified T : DependencyModule, R> FragmentActivity.dependency(noinline resolve: (T) -> R): Lazy<R> = dependency(T::class.java, resolve)
 
 @Deprecated("Use dependency<T, R>(resolve)", ReplaceWith("this.dependency<T, R>(resolve)"), DeprecationLevel.WARNING)
-fun <T : DependencyModule, R> Fragment.dependency(clazz: Class<T>, resolve: T.() -> R): Lazy<R> = lazy { dependencyModule(clazz).resolve() }
+fun <T : DependencyModule, R> Fragment.dependency(clazz: Class<T>, resolve: T.() -> R): Lazy<R> = lazy(LazyThreadSafetyMode.NONE) { dependencyModule(clazz).resolve() }
 inline fun <reified T : DependencyModule, R> Fragment.dependency(noinline resolve: (T) -> R): Lazy<R> = dependency(T::class.java, resolve)
 
 @Deprecated("Use dependency<T, R>(resolve)", ReplaceWith("this.dependency<T, R>(resolve)"), DeprecationLevel.WARNING)
-fun <T : DependencyModule, R> AndroidViewModel.dependency(clazz: Class<T>, resolve: T.() -> R): Lazy<R> = lazy { dependencyModule(clazz).resolve() }
+fun <T : DependencyModule, R> AndroidViewModel.dependency(clazz: Class<T>, resolve: T.() -> R): Lazy<R> = lazy(LazyThreadSafetyMode.NONE) { dependencyModule(clazz).resolve() }
 inline fun <reified T : DependencyModule, R> AndroidViewModel.dependency(noinline resolve: (T) -> R): Lazy<R> = dependency(T::class.java, resolve)
 
 @Deprecated("Use dependency<T, R>(resolve)", ReplaceWith("this.dependency<T, R>(resolve)"), DeprecationLevel.WARNING)
-fun <T : DependencyModule, R> Service.dependency(clazz: Class<T>, resolve: T.() -> R): Lazy<R> = lazy { dependencyModule(clazz).resolve() }
+fun <T : DependencyModule, R> Service.dependency(clazz: Class<T>, resolve: T.() -> R): Lazy<R> = lazy(LazyThreadSafetyMode.NONE) { dependencyModule(clazz).resolve() }
 inline fun <reified T : DependencyModule, R> Service.dependency(noinline resolve: (T) -> R): Lazy<R> = dependency(T::class.java, resolve)
 
 @Deprecated("Use dependency<T, R>(resolve)", ReplaceWith("this.dependency<T, R>(resolve)"), DeprecationLevel.WARNING)
-fun <T : DependencyModule, R> Context.dependency(clazz: Class<T>, resolve: T.() -> R): Lazy<R> = lazy { dependencyModule(clazz).resolve() }
+fun <T : DependencyModule, R> Context.dependency(clazz: Class<T>, resolve: T.() -> R): Lazy<R> = lazy(LazyThreadSafetyMode.NONE) { dependencyModule(clazz).resolve() }
 inline fun <reified T : DependencyModule, R> Context.dependency(noinline resolve: (T) -> R): Lazy<R> = dependency(T::class.java, resolve)
 
 @Deprecated("Use dependency<T, R>(resolve)", ReplaceWith("this.dependency<T, R>(resolve)"), DeprecationLevel.WARNING)
-fun <T : DependencyModule, R> ListenableWorker.dependency(clazz: Class<T>, resolve: T.() -> R): Lazy<R> = lazy { dependencyModule(clazz).resolve() }
+fun <T : DependencyModule, R> ListenableWorker.dependency(clazz: Class<T>, resolve: T.() -> R): Lazy<R> = lazy(LazyThreadSafetyMode.NONE) { dependencyModule(clazz).resolve() }
 inline fun <reified T : DependencyModule, R> ListenableWorker.dependency(noinline resolve: (T) -> R): Lazy<R> = dependency(T::class.java, resolve)
